04941874af0e59490781d45a9919c38c2bfd7de1
Joachim Otáhal <jachym.otahal@gmail.com>
2024-12-06 21:45:47 +0100

    Added middlePoint calculation

--- Fiks2/Program.cs
+++ Fiks2/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        HandleInput handleInput = new HandleInput("input.txt");
+        var points = handleInput.GetPoints(0);
+        
+        // Calculate middle point
+        var middlePointX = 0f;
+        var middlePointY = 0f;
+        // Add up all points
+        foreach (var point in points) {
+            middlePointX += point.X;
+            middlePointY += point.Y;
+        }
+        // Divide by number of points
+        middlePointX /= points.Length; middlePointY /= points.Length;
+        Console.WriteLine($"Middle point: ({middlePointX}, {middlePointY})");
+        
+        
     }
 }
